084f6c3fe225982f210664bbf1cad4d159fd2a9b
stephen-kovolski <s.kovolski@gmail.com>
2021-05-14 21:12:03 -0400

    started making the functions

--- app.js
+++ app.js
@@ -12,5 +12,25 @@ todoButton.addEventListener('click', addTodo)
 //functions
 
 function addTodo(event){
+
+    //prevent form from submitting
+    event.preventDefault();
+
+    //todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //create <li>
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newTodo);
+
+
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerText = '<i class="fas fa-check"></i>'
+
     
 }
