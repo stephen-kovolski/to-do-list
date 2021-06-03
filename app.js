adaa3dea322599f9960a59954d37b0547fa32762
stephen-kovolski <s.kovolski@gmail.com>
2021-06-02 22:28:53 -0400

    JS

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 //selectors
+
+
+    /*The querySelector() method returns the first element that matches a specified CSS selector(s) in the document.
+
+    Note: The querySelector() method only returns the first element that matches the specified selectors. To return all the matches, use the querySelectorAll() method instead.
+
+    If the selector matches an ID in document that is used several times (Note that an "id" should be unique within a page and should not be used more than once), it returns the first matching element.*/ 
+
+    //I used the class as the id in this instance
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
@@ -7,6 +16,22 @@ const todoList = document.querySelector('.todo-list');
 
 
 //event listeners
+    /*The addEventListener() method attaches an event handler to the specified element.
+
+    The addEventListener() method attaches an event handler to an element without overwriting existing event handlers.
+
+    You can add many event handlers to one element.
+
+    You can add many event handlers of the same type to one element, i.e two "click" events.
+
+    You can add event listeners to any DOM object not only HTML elements. i.e the window object.
+
+    The addEventListener() method makes it easier to control how the event reacts to bubbling.
+
+    When using the addEventListener() method, the JavaScript is separated from the HTML markup, for better readability and allows you to add event listeners even when you do not control the HTML markup.
+
+    You can easily remove an event listener by using the removeEventListener() method.
+ */
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck);
 
